b50c6650a183d7b4987ec3e23ab177ad507cdce7
Juan Yidi <juanyiche@gmail.com>
2024-08-23 17:53:32 -0500

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routerUser = require('./user.router.js');
 const routerCategory = require('./category.router.js');
+const routerProduct = require('./product.router.js');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/users', routerUser)
 router.use('/categories', routerCategory)
+router.use('/products', routerProduct)
 
 module.exports = router;
